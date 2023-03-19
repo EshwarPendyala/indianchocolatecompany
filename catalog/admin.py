de42c4a52aa6c82eb6613f3fdc9eaa85aacfa5dd
Eshwar Pendyala <eshwar.pendyala16@vit.edu>
2023-03-19 22:31:21 +0530

    Init dev

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Chocolate, Company, Nutrients
 # Register your models here.
+
+admin.site.register(Chocolate)
+admin.site.register(Company)
+admin.site.register(Nutrients)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
-# Create your models here.
+class Company(models.Model):
+    """Model representing a manufacturer."""
+    name = models.CharField(max_length=100,help_text="Enter a company name.")
+    estb = models.DateField(auto_now=False, auto_now_add=False,help_text="Enter date of establishment.")
+    company_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        """Returns the URL to access a particular company instance."""
+        return reverse('company-detail', args=[str(self.id)])
+    
+class Nutrients(models.Model):
+    """Model representing a nutrients in gram."""
+    nutrient_set_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    calories = models.IntegerField(help_text="Enter calories amount.")
+    protein = models.IntegerField(help_text="Enter protein amount.")
+    fibre = models.IntegerField(help_text="Enter fibre amount.")
+    fat = models.IntegerField(help_text="Enter fat amount.")
+    sugar = models.IntegerField(help_text="Enter sugar amount.")
+    carbohydrates = models.IntegerField(help_text="Enter carbohydrates amount.")
+
+class Chocolate(models.Model):
+    """Model representing chocolate."""
+    chocolate_id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular chocolate.')
+    name = models.CharField(max_length=100,help_text="Enter chocolate name.")
+    manufacturer = models.ForeignKey("Company", on_delete=models.CASCADE)
+    price = models.IntegerField(help_text="Enter Price.")
+    chocolate_type = models.CharField(help_text="Enter type of chocolate.",max_length=50)
+    decription = models.TextField()
+    manufacturing_date = models.DateField(auto_now=False, auto_now_add=False)
+    expiry_date = models.DateField(auto_now=False, auto_now_add=False)
+    nutrients = models.ForeignKey("Nutrients", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+
+    
