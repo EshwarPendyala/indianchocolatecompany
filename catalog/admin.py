ca024b23ec73754f4aa53f539e5b33ff51bcf8d0
Eshwar Pendyala <eshwar.pendyala16@vit.edu>
2023-03-20 23:18:01 +0530

    Add html and changes to views

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from .models import Chocolate, Company, Nutrients
 # Register your models here.
 
-admin.site.register(Chocolate)
-admin.site.register(Company)
-admin.site.register(Nutrients)
+# admin.site.register(Chocolate)
+# admin.site.register(Company)
+# admin.site.register(Nutrients)
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name','estb','company_id')
+
+admin.site.register(Company, CompanyAdmin)
+
+@admin.register(Chocolate)
+class ChocolateAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Nutrients)
+class NutrientsAdmin(admin.ModelAdmin):
+    pass
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Chocolate, Company, Nutrients
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_chocolates = Chocolate.objects.all().count()
+
+    # The 'all()' is implied by default.
+    num_companies = Company.objects.count()
+
+    context = {
+        'num_chocolates': num_chocolates,
+        'num_companies': num_companies,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
